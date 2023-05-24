fa57fac717c8d574153fa804be891f81b51e599a
ByteBane <81875670+bytebane@users.noreply.github.com>
2023-05-25 03:54:36 +0530

    Initial Commit

---
+++ LibrarySystemUI/script.js
@@ -0,0 +1,36 @@
+// Match Password
+function matchPassword(){
+    var pass1=document.form.password.value;
+    var pass2=document.form.confirm_password.value;
+
+    if(pass1 === pass2){
+        return true;
+    }
+    else{
+        alert("Passwords mismatch! Please retry.");
+        return false;
+    }
+}
+// Contact Us link to email with subject
+function contactUs() {
+    if(window.confirm('To Contact us click OK to send an Email to id@domain.com')){
+        window.location.href='mailto://id@domain.com?subject=Need%20Assistance';
+    }
+}
+// Login alert
+function logOut() {
+    alert('You need to login first.');
+}
+
+// Match Password Not-Working
+// function matchPassword(form) {
+//     const password = form.pass.value;
+//     const confirmPassword = form.cPass.value;
+//     if(password.value !== confirmPassword.value) {
+//         confirm_password.setCustomValidity("Passwords Don't Match");
+//         return false;
+//     } else {
+//         confirm_password.setCustomValidity('');
+//         return true;
+//     }
+// }
---
+++ README.md
@@ -0,0 +1,1 @@
+[VisitPage](https://bytebane.github.io/PracticeWebDev)
